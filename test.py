11c3a6a3de7773d882c2d05d611ee04925414767
NostgreSQL <grechnikdiavol@gmail.com>
2023-07-21 23:22:56 +0200

    add label

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print('hi')
+import tkinter as tk
+import time
+
+
+root = tk.Tk()
+root.title("Светофор")
+lable = tk.Label()
+canvas = tk.Canvas(root, width=100, height=250)
+
+
+red_light = canvas.create_oval(25, 20, 75, 70, fill="gray")
+yellow_light = canvas.create_oval(25, 95, 75, 145, fill="gray")
+green_light = canvas.create_oval(25, 170, 75, 220, fill="gray")
+canvas.pack()
+
+root.mainloop()
